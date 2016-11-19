e37bd5116fb0f84957c12433729d68315ce326c5
martin.moraes@gmail.com <Qwerty147!>
2016-11-19 11:25:54 -0200

    Criando o listView. Em implementação.

--- app/src/main/java/com/martin/calculadorasimples/adaptador/AdapterListaResultados.java
+++ app/src/main/java/com/martin/calculadorasimples/adaptador/AdapterListaResultados.java
@@ -1,10 +1,15 @@
 package com.martin.calculadorasimples.adaptador;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import com.martin.calculadorasimples.R;
+import com.martin.calculadorasimples.entidade.Calculo;
 import com.martin.calculadorasimples.persistencia.DbConexao;
 
 /**
@@ -12,9 +17,12 @@ import com.martin.calculadorasimples.persistencia.DbConexao;
  */
 public class AdapterListaResultados extends BaseAdapter {
     private Context context;
+    private LayoutInflater layoutInflater;
 
     public AdapterListaResultados(Context context){
         this.context = context;
+        layoutInflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
@@ -34,6 +42,23 @@ public class AdapterListaResultados extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.item_listview, null);
+        }
+
+        TextView valorUmTextView = (TextView) view.findViewById(R.id.valorUmTextView);
+        TextView valorDoisTextView = (TextView) view.findViewById(R.id.valorDoisTextView);
+        TextView operadorTextView = (TextView) view.findViewById(R.id.operadorTextView);
+        TextView respostaTextView = (TextView) view.findViewById(R.id.RespostaTextView);
+
+        Calculo calculo = (Calculo) getItem(i);
+        Log.d("MEUAPP","Posição: "+ i);
+
+        valorUmTextView.setText(String.valueOf(calculo.getValorUm()));
+        valorDoisTextView.setText(String.valueOf(calculo.getValorDois()));
+        operadorTextView.setText("Não registrado!!!!");
+        respostaTextView.setText(String.valueOf(calculo.getResposta()));
+
+        return view;
     }
 }
